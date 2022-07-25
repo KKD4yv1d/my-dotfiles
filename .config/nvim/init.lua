9268816e3eb52903db675ed0e27e17bbc0dbcb27
KKD4yv1d <kkd4yv1d@gmail.com>
2022-07-25 10:40:31 -0300

    Update neovim config

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,47 +1,31 @@
-package.cpath = package.cpath .. ";" .. "/data/data/com.termux/files/home/.local/lib/?.so"
-require('packer').startup(function (use)
-  use({
-    'wbthomason/packer.nvim',
-    { 'neoclide/coc.nvim', branch = 'release' },
-    'ellisonleao/gruvbox.nvim',
-    'Yggdroot/indentLine',
-    'nvim-treesitter/nvim-treesitter',
-    'ayu-theme/ayu-vim'
-  })
-end)
+require('plugins')
 
-vim.g.indentLine_char = '│'
+-- comma as leader
+vim.g.mapleader = ","
 
-vim.opt.encoding = "utf-8"
-vim.opt.shiftwidth = 2
-vim.opt.tabstop = 2
-vim.opt.softtabstop = 2
-vim.opt.expandtab = true
-vim.opt.autoindent = true
-vim.opt.smartindent = true
-vim.opt.cursorline = true
-vim.opt.showtabline = 2
-vim.opt.incsearch = true
-vim.opt.ignorecase = true
-vim.opt.smartcase = true
-vim.opt.smarttab = true
-vim.opt.number = true
-vim.opt.ruler = true
-vim.opt.scrolloff = 8
-vim.opt.sidescrolloff = 16
-vim.opt.wrap = false
-vim.opt.termguicolors = true
-
-vim.opt.background = "dark"
--- let ayucolor="dark"
+-- load legacy options
 vim.cmd([[
-  colorscheme gruvbox
+	so ~/.config/nvim/legacy.vim
 ]])
 
-require('nvim-treesitter.configs').setup({
-  highlight = { enable = true }
-})
+require('mylsp')
+require('nvimcmp')
 
-vim.cmd('source ' .. vim.fn.expand('~/.config/nvim/vim') .. '/coc-configs.vim')
+-- lsp_signature.nvim
+lsp_signature_cfg = {
+    hint_prefix = "",
+    floating_window = false
+}
+require "lsp_signature".setup(lsp_signature_cfg)
 
-vim.fn.setenv("C_INCLUDE_PATH", vim.fn.getcwd() .. "/include")
+-- lualine
+require('lualine').setup()
+
+-- nvim-treesitter
+require('nvim-treesitter.configs').setup {
+    highlight = {
+        enable = true,
+    },
+}
+
+require('nvim-autopairs').setup {}
---
+++ .config/nvim/lua/nvimcmp.lua
@@ -0,0 +1,83 @@
+-- Setup nvim-cmp.
+local cmp = require 'cmp'
+
+require("cmp").setup({
+  window = {
+    completion = {
+      winhighlight = "Normal:Pmenu,FloatBorder:Pmenu,Search:None",
+      col_offset = -3,
+      side_padding = 0,
+    },
+  },
+  formatting = {
+    fields = { "kind", "abbr", "menu" },
+    format = function(entry, vim_item)
+      local kind = require("lspkind").cmp_format({ mode = "symbol_text", maxwidth = 50 })(entry, vim_item)
+      local strings = vim.split(kind.kind, "%s", { trimempty = true })
+      kind.kind = " " .. strings[1] .. ""
+      kind.menu = "  (" .. strings[2] .. ")"
+
+      return kind
+    end,
+  },
+  snippet = {
+    expand = function(args)
+      vim.fn["vsnip#anonymous"](args.body)
+    end,
+  },
+  mapping = {
+    ['<C-b>'] = cmp.mapping(cmp.mapping.scroll_docs(-4), { 'i', 'c' }),
+    ['<C-f>'] = cmp.mapping(cmp.mapping.scroll_docs(4), { 'i', 'c' }),
+    ['<Tab>'] = cmp.mapping(cmp.mapping.select_next_item(), { 'i', 'c' }),
+    ['<S-Tab>'] = cmp.mapping(cmp.mapping.select_prev_item(), { 'i', 'c' }),
+    ['<C-Space>'] = cmp.mapping(cmp.mapping.complete(), { 'i', 'c' }),
+    ['<C-y>'] = cmp.config.disable, -- Specify `cmp.config.disable` if you want to remove the default `<C-y>` mapping.
+    ['<C-e>'] = cmp.mapping({
+      i = cmp.mapping.abort(),
+      c = cmp.mapping.close(),
+    }),
+    ['<CR>'] = cmp.mapping.confirm({ select = false }), -- Accept currently elected item. Set `select` to `false` to only confirm explicitly selected items.
+  },
+  sources = cmp.config.sources({
+    { name = 'nvim_lsp' },
+    { name = 'vsnip' }, -- For vsnip users.
+    -- { name = 'luasnip' }, -- For luasnip users.
+    -- { name = 'ultisnips' }, -- For ultisnips users.
+    -- { name = 'snippy' }, -- For snippy users.
+  }, {
+    { name = 'buffer' },
+  })
+})
+
+
+
+-- Set configuration for specific filetype.
+cmp.setup.filetype('gitcommit', {
+  sources = cmp.config.sources({
+    { name = 'cmp_git' }, -- You can specify the `cmp_git` source if you were installed it.
+  }, {
+    { name = 'buffer' },
+  })
+})
+
+-- Use buffer source for `/` (if you enabled `native_menu`, this won't work anymore).
+cmp.setup.cmdline('/', {
+  sources = {
+    { name = 'buffer' }
+  }
+})
+
+-- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
+cmp.setup.cmdline(':', {
+  sources = cmp.config.sources({
+    { name = 'path' }
+  }, {
+    { name = 'cmdline' }
+  })
+})
+
+-- Setup lspconfig.
+-- local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
+-- require('lspconfig')['clangd'].setup {
+--   capabilities = capabilities
+-- }
